71e8d8f60ed70b6e96d48794ff61a33f00b9f742
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-05-02 16:20:29 +0100

    navbar hide fixed

--- app/components/navbar/page.tsx
+++ app/components/navbar/page.tsx
@@ -22,30 +22,31 @@ const Navbar = () => {
     }
     return (
         <>
-            <nav className="flex w-full items-center justify-between navbar bg-base-100 p-lengthMd2">
+            <nav className="flex w-full items-center justify-between navbar bg-base-100 p-lengthMd2 sticky top-0 z-10">
 
                 <div className="navbar-center">
                     <Link href="/"><Image src={Logo} alt='logo' height={80} width={120} /></Link>
 
                 </div>
                 <div><button role="button" className='btn bg-caribbean text-sand' onClick={calendlyLink}>Book a Call</button></div>
-                <div className="navbar-center sm:hidden md:hidden  lg:hidden xl:block">
+                <div className="navbar-center hidden xl:block">
 
                     {link.map(link => (
                         <Link href={link.url} key={link.id} className=" flex-row  justify-between  px-lengthSm3"  >{link.title}</Link>
                     ))}
                 </div>
 
-                <div className="navbar-end sm:block md:hidden lg:hidden">
+                <div className="navbar-end xl:hidden">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ">
-                            <li className=' flex flex-nowrap'> {link.map(link => (
-                                <Link href={link.url} key={link.title}>{link.title}</Link>
-                            ))}</li>
+                            {link.map(link => (
+                                <li className=' flex flex-nowrap'>
+                                    <Link href={link.url} key={link.title}>{link.title}</Link>
+                                </li>))}
 
                         </ul>
                     </div>
