fbdf4ffba7687957f3a2b19c5aca71e86be20ae5
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-05-12 23:10:42 +0100

    dark theme fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: false,
+
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
