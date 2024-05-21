7e3b822b1521cf8a2c5017f73e473baa26252279
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-05-21 09:06:35 +0100

    fixed deployment issue

--- app/components/CallToAction.tsx
+++ app/components/CallToAction.tsx
@@ -1,12 +1,12 @@
-"use client"
-import React from 'react'
-import ContactForm from '../components/contact-us/components/ContactForm'
-import { Client } from 'undici-types'
+// // "use client"
+// import React from 'react'
+// // import ContactForm from '../components/contact-us/components/ContactForm'
+// import { Client } from 'undici-types'
 
-const CallToAction = () => {
-    return (
-        <div><button onClick={() => ContactForm}>Contact Us</button></div>
-    )
-}
+// const CallToAction = () => {
+//     return (
+//         <div><button onClick={() => ContactForm}>Contact Us</button></div>
+//     )
+// }
 
-export default CallToAction
+// export default CallToAction
