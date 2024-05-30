6dfa458fb779c8b93f4130c66fcf6602a4b6a4cc
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-05-30 13:47:45 +0100

    updated aout us and tidied up

--- app/components/about-us/page.tsx
+++ app/components/about-us/page.tsx
@@ -1,52 +1,89 @@
 import React from 'react';
+import Image from 'next/image';
 import Hero from '../hero/page'
 import CallToAction from '../../CallToAction';
 import aboutUs from './aboutUsData.json'
+
+
 
 interface Service {
     tag: string;
     mission: string;
     who: string[];
-    what: string[];
+    what: string;
     how: { title: string; description: string }[];
     choice: string;
     values: string[];
+
 }
 
 const AboutUs: React.FC = () => {
 
     const data = aboutUs.aboutUsData;
+
     return (
         <>
-            <section className="dark:bg-gray-100 dark:text-gray-800">
-                <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
-                    <h1 className="mb-12 text-h1 font-oswald font-bold leading-none text-center ">About Us</h1>
-                    <Hero />
-                    <div className="divide-y dark:divide-gray-300 font-roboto">
-                        <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
-                            <h3 className="font-semibold md:col-span-5">Who We Are:</h3>
-                            <p className="md:pl-0 md:col-span-7">{data[1].who}</p>
-                        </div>
-                        <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
-                            <h3 className="font-semibold md:col-span-5">What We Do:</h3>
-                            <ul className="md:pl-0 md:col-span-7">{data[2].what}
-                                {data[3].how.map((list, index) =>
-                                    (<li className=' list-disc' key={index}>{list.title}: {list.description}</li>))}
+            <section>
+                <h1 className="font-oswald text-h1 text-center">About Us</h1>
+                <div className="w-full card lg:card-side bg-base-100 shadow-xl font-roboto text-purple dark:text-champagne">
+                    <figure className="w-full"><Image src="/service/mission.jpeg" alt="crafted boat with mission values purpose in wooden blocks" width={500} height={500} /></figure>
+                    <div className="card-body ">
+                        <h2 className="card-title text-h2">{data[0].tag}</h2>
+                        <h3 className="card-title">Our Mission:</h3> <span className="text-h4">{data[0].mission}</span>
 
-                            </ul>
+
+
+                        <div className="card-actions justify-start">
+                            <h2 className="card-title">Who We Are:
+                            </h2><br />
+                            <h3 className="text-h4  ">{data[2].what}</h3>
+
                         </div>
-                        <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
-                            <h3 className="font-semibold md:col-span-5">Our Values</h3>
-                            <ul>
-                                {data[5].values.map((value, index) => (<li className="list-disc" key={index}>{value}</li>))}
-                            </ul>
+                        <div>
+                            <ol className=" list-inside">
+                                {data[3].how.map((item, index) => (
+                                    <li className="text-h4 list-style-position: inside  px-lengthSm1 list-disc " key={index}><span className='text-cyan'>{item.title}-</span>{''}{item.description}</li>
+
+
+                                ))}
+
+                            </ol>
                         </div>
 
                     </div>
+                </div >
+                <div className="w-full card lg:card-side bg-base-100 shadow-xl font-roboto text-purple dark:text-champagne">
+                    <div className="card-body ">
+                        <h2 className="card-title text-h2">{data[4].choice}</h2>
+
+
+
+
+                        <div className="card-actions justify-start">
+                            <h2 className="card-title">Our values:
+                            </h2><br />
+
+
+                            <ol className=" list-inside">
+                                {data[5].values.map((value, index) => (
+                                    <li className="text-h4 list-style-position: inside  px-lengthSm1 list-disc " key={index}>{value}</li>
+
+
+                                ))}
+
+                            </ol>
+                        </div>
+                        <div className="card-actions justify-start">
+                            <h2 className="card-title">Ready to Launch Your Startup Journey?
+                            </h2><br />
+                            <h3 className="text-h4  ">Schedule a free call to discuss your startup goals.</h3>
+                            <CallToAction />
+
+                        </div>
+                    </div>
+
                 </div>
-                <div>{data[4].choice}</div>
-                <div><CallToAction /></div>
-            </section>
+            </section >
         </>
     )
 }
