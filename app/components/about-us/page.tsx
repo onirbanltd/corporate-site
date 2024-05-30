a638c477ba24629b293c414bd8e34dca157b9b68
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-05-30 18:23:08 +0100

    deplyment fixed for data access

--- app/components/about-us/page.tsx
+++ app/components/about-us/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import Hero from '../hero/page'
+
 import CallToAction from '../../CallToAction';
 import aboutUs from './aboutUsData.json'
 
@@ -20,8 +20,16 @@ interface Service {
 const AboutUs: React.FC = () => {
 
     const data = aboutUs.aboutUsData;
+    const tag = data[0]?.tag || "";
+    const mission = data[0]?.mission || "";
+    const who = data[1]?.who || "";
+    const what = data[2]?.what || "";
+    const how = data[3]?.how || [];
+    const choice = data[4]?.choice || "";
+    const values = data[5]?.values || [];
 
     return (
+
         <>
             <section>
                 <h1 className="font-oswald text-h1 text-center">About Us</h1>
@@ -31,21 +39,23 @@ const AboutUs: React.FC = () => {
                         <div className="w-full card lg:card-side bg-base-100 shadow-xl font-roboto text-purple dark:text-champagne">
                             <figure className="w-full"><Image src="/service/mission.jpeg" alt="crafted boat with mission values purpose in wooden blocks" width={500} height={500} /></figure>
                             <div className="card-body ">
-                                <h2 className="card-title text-h2">{data[0].tag}</h2>
-                                <h3 className="card-title">Our Mission:</h3> <span className="text-h4">{data[0].mission}</span>
+                                <h2 className="card-title text-h2">{tag}</h2>
+                                <h3 className="card-title">Our Mission:</h3> <span className="text-h4">{mission}</span>
 
 
 
                                 <div className="card-actions justify-start">
                                     <h2 className="card-title">Who We Are:
-                                    </h2><br />
-                                    <h3 className="text-h4  ">{data[2].what}</h3>
+                                    </h2>
+                                    <h3 className="text-h4  ">{who}</h3>
 
                                 </div>
                                 <div>
+                                    <h3 className='text-h4'> {what}</h3>
                                     <ol className=" list-inside">
-                                        {data[3].how.map((item, index) => (
-                                            <li className="text-h4 list-style-position: inside  px-lengthSm1 list-disc " key={index}><span className='text-cyan'>{item.title}-</span>{''}{item.description}</li>
+                                        {how.map((item, index) => (
+                                            <li className="text-h4 list-style-position: inside  px-lengthSm1 list-disc " key={index}><span className='text-cyan'>{item.title}-
+                                            </span>{''}{item.description}</li>
 
 
                                         ))}
@@ -57,7 +67,7 @@ const AboutUs: React.FC = () => {
                         </div >
                         <div className="w-full card lg:card-side bg-base-100 shadow-xl font-roboto text-purple dark:text-champagne">
                             <div className="card-body ">
-                                <h2 className="card-title text-h2">{data[4].choice}</h2>
+                                <h2 className="card-title text-h2">{choice}</h2>
 
 
 
@@ -68,23 +78,29 @@ const AboutUs: React.FC = () => {
 
 
                                     <ol className=" list-inside">
-                                        {data[5].values.map((value, index) => (
+                                        {values.map((value, index) => (
                                             <li className="text-h4 list-style-position: inside  px-lengthSm1 list-disc " key={index}>{value}</li>
 
 
                                         ))}
 
                                     </ol>
-                                </div>
-                                <div className="card-actions justify-start">
-                                    <h2 className="card-title">Ready to Launch Your Startup Journey?
-                                    </h2><br />
-                                    <h3 className="text-h4  ">Schedule a free call to discuss your startup goals.</h3>
-                                    <CallToAction />
+
 
                                 </div>
                             </div>
 
+                        </div>
+                        <div className="w-full card lg:card-side bg-base-100 shadow-xl font-roboto text-purple dark:text-champagne">
+
+                            <figure className="w-full"><Image src="/service/nextsteps.jpeg" alt="Next steps for product development start up book call Onirban" width={500} height={500} /></figure>
+                            <div className="card-body justify-start">
+                                <h2 className="card-title text-h2">Ready to Launch Your Startup Journey?
+                                </h2>
+                                <h3 className="text-h3  ">Schedule a free call to discuss your startup goals.</h3>
+                                <CallToAction />
+
+                            </div>
                         </div>
                     </>)}
                 {!data && <span className="loading loading-infinity loading-md"></span>}
